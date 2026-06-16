82624d8a710c5ff364ad4994d26cc362b203648d
dheredia_globant <diego.heredia@globant.com>
2026-06-16 10:55:37 -0300

    Fix para enviar el empleado por page de Empleado

--- src/pages/EmpleadoPage.tsx
+++ src/pages/EmpleadoPage.tsx
@@ -1,10 +1,13 @@
+// src/pages/EmpleadoPage.tsx
 import React from "react";
 import { LandingContainer } from "../components/LandingContainer";
+import { useAuthStore } from "../store/auth.store";
 
 export const EmpleadoPage: React.FC = () => {
-  return (
-    <>
-      <LandingContainer />
-    </>
-  );
+  const empleado = useAuthStore((s) => s.empleado);
+  const nombre = empleado
+    ? `${empleado.nombre} ${empleado.apellido}`
+    : "Usuario";
+
+  return <LandingContainer username={nombre} />;
 };
